6b1f99dffb5b4eadcd0d4bda76cedc0a93f3a881
Sebastián Rojo <sebastianrojocollao@gmail.com>
2021-11-14 18:05:22 -0300

    Commit inicial

---
+++ src/main/java/cl/sebastianrojo/springsecuritybodega/SpringSecurityBodegaApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastianrojo.springsecuritybodega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBodegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBodegaApplication.class, args);
+	}
+
+}
